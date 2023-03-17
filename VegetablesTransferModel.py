092436b335283dd66565f1ace684c59322268677
Adrien <adr.bertrand@gmail.com>
2023-03-17 11:15:25 -0400

    created model on top of ResNet50V2

---
+++ VegetablesTransferModel.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow import keras
+
+trained_model = tf.keras.applications.ResNet50V2(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=1000,
+    classifier_activation="softmax",
+)
+
+trained_model.trainable = False
+
+inputs = keras.Input(shape=(150, 150, 3))
+# We make sure that the base_model is running in inference mode here,
+# by passing `training=False`. This is important for fine-tuning, as you will
+# learn in a few paragraphs.
+x = trained_model(inputs, training=False)
+# Convert features of shape `base_model.output_shape[1:]` to vectors
+x = keras.layers.GlobalAveragePooling2D()(x)
+# A Dense classifier with a single unit (binary classification)
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
